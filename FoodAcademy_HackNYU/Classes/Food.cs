0621c84e3988e48ee7b5e8f9e63f093898c0c68e
Zhongheng Li <heng2j@gmail.com>
2017-02-18 08:54:53 -0500

    first commit

---
+++ FoodAcademy_HackNYU/Classes/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UIKit;
+
+namespace FoodAcademy_HackNYU
+{
+	public class Food
+	{
+
+		public string name { get; set; }
+		public int calories { get; set; }
+		public int fat { get; set; }
+		public int protein { get; set; }
+		public int fiber { get; set; }
+		public UIImage image { get; set; }
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# FoodAcademy---HackNYU2017
